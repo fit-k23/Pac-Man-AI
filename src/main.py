928639254aaffd59bb64fe032127508648cec6fa
hanhtrang256 <147674017+hanhtrang256@users.noreply.github.com>
2025-02-26 11:53:23 +0700

    [ADD] draw grid and maze

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import pygame
+from map import Map
+
+# Screen and Block paramters
+BLOCK_SIZE = 25
+SCREEN_WIDTH = BLOCK_SIZE * 28
+SCREEN_HEIGHT = BLOCK_SIZE * 30
+OFFSET_BLOCK = 2
+
+# Color 
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+# Map object
+maze = Map.parse("../maps/map_02.txt")
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # RENDER YOUR GAME HERE
+    
+    maze.drawGrid(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
+    maze.drawMap(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE, OFFSET_BLOCK)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
--- src/map.py
+++ src/map.py
@@ -1,3 +1,11 @@
+import pygame
+
+# Color 
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
 class Map:
     data = []
     @staticmethod
@@ -5,5 +13,19 @@ class Map:
         _map = Map()
         with open(file_path, 'r', encoding='utf-8') as f:
             for line in f.readlines():
-                _map.data.append([*line])
+                _map.data.append([*line.strip('\n')])
         return _map
+    def drawGrid(self, screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE):
+        for x in range(0, SCREEN_WIDTH, BLOCK_SIZE):
+            for y in range(0, SCREEN_HEIGHT, BLOCK_SIZE):
+                rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+                pygame.draw.rect(screen, WHITE, rect, 1)
+    def drawMap(self, screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE, OFFSET_BLOCK):
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                if self.data[i][j] == '*':
+                    rect = pygame.Rect(j * BLOCK_SIZE, i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+                    pygame.draw.rect(screen, BLUE, rect, 0)
+                elif self.data[i][j] == '.':
+                    rect = pygame.Rect(j * BLOCK_SIZE + BLOCK_SIZE / 2 - OFFSET_BLOCK, i * BLOCK_SIZE + BLOCK_SIZE / 2 - OFFSET_BLOCK, 5, 5)
+                    pygame.draw.rect(screen, RED, rect, 0)
