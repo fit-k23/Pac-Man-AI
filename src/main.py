b5bd0bd3c9408fc62edef22f32aae4ea8ce72499
hanhtrang256 <147674017+hanhtrang256@users.noreply.github.com>
2025-02-27 11:43:49 +0700

    [UPDATE] Draw map with curves

--- src/main.py
+++ src/main.py
@@ -1,17 +1,10 @@
 import pygame
-from map import Map
+from map import *
 
-# Screen and Block paramters
-BLOCK_SIZE = 25
-SCREEN_WIDTH = BLOCK_SIZE * 28
-SCREEN_HEIGHT = BLOCK_SIZE * 30
-OFFSET_BLOCK = 2
-
-# Color 
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
+# Screen, Block paramters
+BLOCK_SIZE = 20
+SCREEN_WIDTH = BLOCK_SIZE * 30
+SCREEN_HEIGHT = BLOCK_SIZE * 33
 
 # pygame setup
 pygame.init()
@@ -33,9 +26,14 @@ while running:
     screen.fill("black")
 
     # RENDER YOUR GAME HERE
-    
-    maze.drawGrid(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
-    maze.drawMap(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE, OFFSET_BLOCK)
+    # maze.drawGrid(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
+    maze.drawMap(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
+
+    # for i in range(len(maze.data)):
+    #     for j in range(len(maze.data[i])):
+    #         print(maze.data[i][j], end="")
+    #     print()
+    # break
 
     # flip() the display to put your work on screen
     pygame.display.flip()
--- src/map.py
+++ src/map.py
@@ -1,10 +1,9 @@
 import pygame
+import math
+from color import *
 
-# Color 
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
+# THICKNESS
+THICK = 4
 
 class Map:
     data = []
@@ -13,19 +12,41 @@ class Map:
         _map = Map()
         with open(file_path, 'r', encoding='utf-8') as f:
             for line in f.readlines():
-                _map.data.append([*line.strip('\n')])
+                str = line.strip('\n').replace(" ", "")
+                _map.data.append(str)
         return _map
     def drawGrid(self, screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE):
         for x in range(0, SCREEN_WIDTH, BLOCK_SIZE):
             for y in range(0, SCREEN_HEIGHT, BLOCK_SIZE):
                 rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
                 pygame.draw.rect(screen, WHITE, rect, 1)
-    def drawMap(self, screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE, OFFSET_BLOCK):
+    def drawMap(self, screen, SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE):
+        off_x = 1
+        off_y = 1
+        off_block = 2
         for i in range(len(self.data)):
             for j in range(len(self.data[i])):
-                if self.data[i][j] == '*':
-                    rect = pygame.Rect(j * BLOCK_SIZE, i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
-                    pygame.draw.rect(screen, BLUE, rect, 0)
-                elif self.data[i][j] == '.':
-                    rect = pygame.Rect(j * BLOCK_SIZE + BLOCK_SIZE / 2 - OFFSET_BLOCK, i * BLOCK_SIZE + BLOCK_SIZE / 2 - OFFSET_BLOCK, 5, 5)
-                    pygame.draw.rect(screen, RED, rect, 0)
+                x = j * BLOCK_SIZE
+                y = i * BLOCK_SIZE
+                # 5, 6, 7, 8: arc
+                if self.data[i][j] == '6':
+                    rect = pygame.Rect(x + BLOCK_SIZE / 2 - off_x, y + BLOCK_SIZE / 2 - off_y, BLOCK_SIZE + off_block, BLOCK_SIZE + off_block)
+                    pygame.draw.arc(screen, BLUE, rect, math.radians(90), math.radians(180), THICK)
+                elif self.data[i][j] == '8':
+                    rect = pygame.Rect(x - BLOCK_SIZE / 2 + off_x, y - BLOCK_SIZE / 2 + off_y, BLOCK_SIZE + off_block, BLOCK_SIZE + off_block)
+                    pygame.draw.arc(screen, BLUE, rect, math.radians(270), math.radians(360), THICK)
+                elif self.data[i][j] == '5':
+                    rect = pygame.Rect(x - BLOCK_SIZE / 2 + off_x, y + BLOCK_SIZE / 2 - off_y, BLOCK_SIZE + off_block, BLOCK_SIZE + off_block)
+                    pygame.draw.arc(screen, BLUE, rect, math.radians(0), math.radians(90), THICK)
+                elif self.data[i][j] == '7':
+                    rect = pygame.Rect(x + BLOCK_SIZE / 2 - off_x, y - BLOCK_SIZE / 2 + off_y, BLOCK_SIZE + off_block, BLOCK_SIZE + off_block)
+                    pygame.draw.arc(screen, BLUE, rect, math.radians(180), math.radians(270), THICK)
+                # 3, 4: straight line
+                elif self.data[i][j] == '3':
+                    startPos = (x + BLOCK_SIZE / 2, y)
+                    endPos = (x + BLOCK_SIZE / 2, y + BLOCK_SIZE)
+                    pygame.draw.line(screen, BLUE, startPos, endPos, THICK)
+                elif self.data[i][j] == '4':
+                    startPos = (x, y + BLOCK_SIZE / 2)
+                    endPos = (x + BLOCK_SIZE, y + BLOCK_SIZE / 2)
+                    pygame.draw.line(screen, BLUE, startPos, endPos, THICK)
